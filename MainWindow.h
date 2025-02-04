c3fbda12ebef49edbd4ecb462fa068e3e5f0b6de
foglands <penfankd@gmail.com>
2025-02-04 17:45:32 -0500

    Create MainWindow.h

---
+++ MainWindow.h
@@ -0,0 +1,18 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "GameWindow.h"
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr); // Constructor
+    ~MainWindow(); // Destructor
+
+private:
+    GameWindow *gameWindow; // Pointer to game window instance
+};
+
+#endif // MAINWINDOW_H
