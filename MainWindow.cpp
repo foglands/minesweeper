9f854ac50475eb36796519992c7a432aa10ad529
foglands <penfankd@gmail.com>
2025-02-04 17:45:49 -0500

    Create MainWindow.cpp

---
+++ MainWindow.cpp
@@ -0,0 +1,14 @@
+#include "MainWindow.h"
+
+// Constructor: Initializes the main window and game window
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    gameWindow = new GameWindow(this);
+    setCentralWidget(gameWindow);
+    setWindowTitle("Minesweeper"); // Set the window title
+    resize(900, 500); // Set the initial window size
+}
+
+// Destructor: Cleans up dynamically allocated objects
+MainWindow::~MainWindow() {
+    delete gameWindow;
+}
