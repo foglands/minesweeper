53f7f6d4052d5f4cbe819613d7d92f3574a76833
foglands <penfankd@gmail.com>
2025-02-05 17:24:48 -0500

    Update MineGrid.cpp

--- MineGrid.cpp
+++ MineGrid.cpp
@@ -1,12 +1,17 @@
+// MineGrid.cpp
+// Implements game logic, such as mine placement and win/loss conditions.
+
 #include "MineGrid.h"
 #include <cstdlib>
 #include <ctime>
 
+// Constructor: Initializes the grid with mines
 MineGrid::MineGrid(int width, int height, int mines)
     : width(width), height(height), mineCount(mines) {
     resetGrid();
 }
 
+// Resets the game grid
 void MineGrid::resetGrid() {
     grid.assign(width, std::vector<int>(height, 0));
     revealed.assign(width, std::vector<bool>(height, false));
@@ -14,6 +19,7 @@ void MineGrid::resetGrid() {
     generateMines();
 }
 
+// Generates mines randomly on the grid
 void MineGrid::generateMines() {
     srand(time(nullptr));
     int placedMines = 0;
@@ -27,6 +33,7 @@ void MineGrid::generateMines() {
     }
 }
 
+// Checks if a tile contains a mine
 bool MineGrid::isMine(int x, int y) {
     return grid[x][y] == -1;
 }
