5e68c6600eff243785946ae091d00e8e82d9fc61
foglands <penfankd@gmail.com>
2025-02-05 19:27:56 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,9 @@
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
-    // Initialize the Qt application
     QApplication app(argc, argv);
-    
-    // Create and display the main window
     MainWindow window;
     window.show();
-    
-    // Start the event loop
     return app.exec();
 }
+
