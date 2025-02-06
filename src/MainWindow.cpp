d69b82f07690f0c73f5c94c66c851da1f3c1e2e1
foglands <penfankd@gmail.com>
2025-02-05 20:30:06 -0500

    Update MainWindow.cpp

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,57 +1,63 @@
 #include "MainWindow.h"
-#include "GameWindow.h"
+#include <QApplication>
 
-#include <QMenuBar>
-#include <QMenu>
-#include <QAction>
-
-/*
- * MainWindow Constructor:
- * - Initializes the central widget (GameWindow).
- * - Sets up the menu bar with "New Game" and "Exit" options.
+/**
+ * @brief Constructor for MainWindow.
+ * @param parent The parent QWidget.
  */
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    // Create the game window and set it as the central widget.
     gameWindow = new GameWindow(this);
     setCentralWidget(gameWindow);
 
-    // Create a "Game" menu in the menu bar.
-    QMenu *gameMenu = menuBar()->addMenu(tr("&Game"));
+    setupMenuBar();
+    setWindowTitle("Minesweeper");
+    resize(900, 500);
+}
 
-    // Create "New Game" and "Exit" actions.
-    QAction *newGameAction = new QAction(tr("&New Game"), this);
-    QAction *exitAction = new QAction(tr("E&xit"), this);
+/**
+ * @brief Destructor for MainWindow.
+ */
+MainWindow::~MainWindow()
+{
+    delete gameWindow;
+}
 
-    // Add actions to the menu.
+/**
+ * @brief Initializes the menu bar with actions.
+ */
+void MainWindow::setupMenuBar()
+{
+    menuBar = new QMenuBar(this);
+    setMenuBar(menuBar);
+
+    QMenu *gameMenu = menuBar->addMenu("Game");
+
+    // Create "New Game" action
+    newGameAction = new QAction("New Game", this);
+    connect(newGameAction, &QAction::triggered, this, &MainWindow::startNewGame);
     gameMenu->addAction(newGameAction);
-    gameMenu->addSeparator();
+
+    // Create "Exit" action
+    exitAction = new QAction("Exit", this);
+    connect(exitAction, &QAction::triggered, this, &MainWindow::exitGame);
     gameMenu->addAction(exitAction);
-
-    // Connect menu actions to corresponding slots.
-    connect(newGameAction, &QAction::triggered, this, &MainWindow::newGame);
-    connect(exitAction, &QAction::triggered, this, &MainWindow::close);
-
-    setWindowTitle(tr("Minesweeper"));
-    resize(800, 600);
 }
 
-/*
- * Destructor: Cleanup is handled automatically since gameWindow is a child widget.
+/**
+ * @brief Slot function to start a new game.
  */
-MainWindow::~MainWindow() {
+void MainWindow::startNewGame()
+{
+    gameWindow->resetGame();
 }
 
-/*
- * Slot to start a new game:
- * Deletes the current GameWindow and creates a new one.
+/**
+ * @brief Slot function to exit the application.
  */
-void MainWindow::newGame() {
-    if (gameWindow) {
-        gameWindow->deleteLater();
-    }
-    gameWindow = new GameWindow(this);
-    setCentralWidget(gameWindow);
+void MainWindow::exitGame()
+{
+    QApplication::quit();
 }
+
 
